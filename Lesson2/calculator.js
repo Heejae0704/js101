8d33833e23ed024a75d487cfb701c9d0f5ac66e8
heejaechang <heejae0704@gmail.com>
2020-03-14 19:09:54 +0900

    implemented calculation bonus features

--- Lesson2/calculator.js
+++ Lesson2/calculator.js
@@ -1,3 +1,5 @@
+const mesObj = require('./mes');
+
 const readline = require("readline-sync");
 
 function prompt(message) {
@@ -8,56 +10,76 @@ function invalidNumber(number) {
   return String(number).trimStart() === "" || Number.isNaN(number);
 }
 
-prompt("Welcome to Calculator!");
+let mes = mesObj.en;
 
-prompt("Please enter the first number:");
-let num1 = Number(readline.question());
+prompt("Select language (en/ko): ");
+  let lan = readline.question().toLowerCase();
+  while (!(lan === "en" || lan === "ko")) {
+    prompt("Select correct language! (en/ko): ");
+    lan = readline().toLowerCase();
+  }
 
-while (invalidNumber(num1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  num1 = Number(readline.question());
-}
+  if (lan === "en") {
+    mes = mesObj.en;
+  } else if (lan === "ko") {
+    mes = mesObj.ko;
+  }
 
-prompt("Please enter the second number:");
-let num2 = Number(readline.question());
+prompt(mes.welcome);
+let cont;
 
-while (invalidNumber(num2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  num2 = Number(readline.question());
-}
+do {
+  prompt(mes.firstNum);
+  let num1 = Number(readline.question());
+  while (invalidNumber(num1)) {
+    prompt(mes.NumError);
+    num1 = Number(readline.question());
+  }
 
-console.log(`${num1} ${num2}`);
+  prompt(mes.secondNum);
+  let num2 = Number(readline.question());
+  while (invalidNumber(num2)) {
+    prompt(mes.NumError);
+    num2 = Number(readline.question());
+  }
 
-prompt(
-  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
-);
-let operator = readline.question();
+  console.log(`${num1} ${num2}`);
 
-while (!["1", "2", "3", "4"].includes(operator)) {
-  prompt("Must choose 1, 2, 3 or 4");
-  operator = readline.question();
-}
+  prompt(mes.operation);
+  let operator = readline.question();
+  while (!["1", "2", "3", "4"].includes(operator)) {
+    prompt("Must choose 1, 2, 3 or 4");
+    operator = readline.question();
+  }
 
-let output = 0;
-switch (operator) {
-  case "1":
-    prompt("Performing add operation");
-    output = num1 + num2;
-    break;
-  case "2":
-    prompt("Performing subtract operation");
-    output = num1 - num2;
-    break;
-  case "3":
-    prompt("Performing multiply operation");
-    output = num1 * num2;
-    break;
-  case "4":
-    prompt("Performing divide operation");
-    output = num1 / num2;
-    break;
-  default:
-    break;
-}
+  let output = 0;
+  switch (operator) {
+    case "1":
+      prompt(mes.add);
+      output = num1 + num2;
+      break;
+    case "2":
+      prompt(mes.subtract);
+      output = num1 - num2;
+      break;
+    case "3":
+      prompt(mes.multiply);
+      output = num1 * num2;
+      break;
+    case "4":
+      prompt(mes.divide);
+      output = num1 / num2;
+      break;
+    default:
+      break;
+  }
 
-prompt(`The result is: ${output}`);
+  prompt(mes.result + `${output.toFixed(2)}`);
+  prompt(mes.cont);
+  cont = readline.question().toLowerCase();
+  while (!(cont === "yes" || cont === "no" || cont === "y" || cont === "n")) {
+    prompt(mes.contError);
+    cont = readline.question().toLowerCase();
+  }
+
+} while (cont === "yes" || cont === "y");
