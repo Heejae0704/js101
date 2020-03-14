e2da9160569b7ef7171e06c44be27d6e293b9249
heejaechang <heejae0704@gmail.com>
2020-03-14 13:23:43 +0900

    up to repackaging calculator

--- Lesson2/calculator.js
+++ Lesson2/calculator.js
@@ -1,43 +1,63 @@
-// Ask the user for the first number.
-
-// Ask the user for the second number.
-
-// Ask the user for an operation to perform.
-
-// Perform the operation on the two numbers.
-
-// Print the result to the terminal.
-
 const readline = require("readline-sync");
 
-console.log("Welcome to Calculator!");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-const num1 = Number(readline.question("Please enter the first number:\n"));
-const num2 = Number(readline.question("Please enter the first number:\n"));
+function invalidNumber(number) {
+  return String(number).trimStart() === "" || Number.isNaN(number);
+}
+
+prompt("Welcome to Calculator!");
+
+prompt("Please enter the first number:");
+let num1 = Number(readline.question());
+
+while (invalidNumber(num1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  num1 = Number(readline.question());
+}
+
+prompt("Please enter the second number:");
+let num2 = Number(readline.question());
+
+while (invalidNumber(num2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  num2 = Number(readline.question());
+}
+
 console.log(`${num1} ${num2}`);
-const operator = readline.question(
-  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n"
+
+prompt(
+  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
 );
+let operator = readline.question();
+
+while (!["1", "2", "3", "4"].includes(operator)) {
+  prompt("Must choose 1, 2, 3 or 4");
+  operator = readline.question();
+}
+
 let output = 0;
 switch (operator) {
   case "1":
-    console.log("Performing add operation");
+    prompt("Performing add operation");
     output = num1 + num2;
     break;
   case "2":
-    console.log("Performing subtract operation");
+    prompt("Performing subtract operation");
     output = num1 - num2;
     break;
   case "3":
-    console.log("Performing multiply operation");
+    prompt("Performing multiply operation");
     output = num1 * num2;
     break;
   case "4":
-    console.log("Performing divide operation");
+    prompt("Performing divide operation");
     output = num1 / num2;
     break;
   default:
     break;
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
